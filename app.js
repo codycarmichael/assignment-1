fb8fe5a5ca14ac481a960590d85335be13f0653e
Cody Carmichael <138245267+codycarmichael@users.noreply.github.com>
2023-09-10 09:34:43 -0600

    changed route reference

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const http = require('http');
-const routes  = require('codycarmichael/assignment-1/routes.js');
+const routes  = require('https://github.com/codycarmichael/assignment-1/blob/main/routes.js');
 const server = http.createServer(routes);
 server.listen(3000);
